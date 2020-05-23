d6e59c48c41501119ed5ec9fdf313df01235b046
AnnaYablonskaya <anna.yablonskaya2002@yandex.ru>
2020-05-23 14:27:43 +0300

    Transfering 2

--- bot.py
+++ bot.py
@@ -86,16 +86,15 @@ def text_handler(message):
         bot.register_next_step_handler(msg_out, ask_internet_number)
 
 
+
     elif message.text == 'Transfer from card to card':
-        msg = 'Сhoose a card from which you want to transfer money'
-        bot.send_message(message.chat.id, msg)
+        msg = 'Enter amount you want to transfer'
         telegram_id = message.chat.id
         payment_type = message.text
         payment = Payment(telegram_id, payment_type)
         user_payment[telegram_id] = payment
-
-        msg_out = menu.card_menu(bot, message.chat.id, int(payment.payment_amount))
-        bot.register_next_step_handler(msg_out, ask_card)
+        msg_out = bot.send_message(message.chat.id, msg)
+        bot.register_next_step_handler(msg_out, ask_card_sum)
 
 
     elif message.text == 'Main menu':
@@ -189,11 +188,12 @@ def ask_card(message):
     message.text.find(':')
     payment_card = message.text[0: message.text.find(':')]
     payment.payment_card = payment_card
+
     card.subtracting_from_card(bot, telegram_id, payment_card, int(payment.payment_amount))
 
-    msg = 'Enter amount'
+    msg = 'Сhoose a card on which you want to transfer money'
     msg_out = bot.send_message(message.chat.id, msg)
-    bot.register_next_step_handler(msg_out, ask_card_sum)
+    bot.register_next_step_handler(msg_out, ask_card_adding)
 
 
 def ask_card_sum(message):
@@ -208,10 +208,11 @@ def ask_card_sum(message):
     payment = user_payment[telegram_id]
     payment.payment_amount = card_sum
 
-    msg = 'Сhoose a card on which you want to transfer money'
+    msg = 'Сhoose a card from which you want to transfer money'
     bot.send_message(message.chat.id, msg)
+
     msg_out = menu.card_menu(bot, message.chat.id, int(payment.payment_amount))
-    bot.register_next_step_handler(msg_out, ask_card_adding)
+    bot.register_next_step_handler(msg_out, ask_card)
 
 
 def ask_card_adding(message):
